c713753efdc557e101d65f836b6666f116aad360
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-21 09:15:51 +0530

    dequeue  from the beginning

---
+++ Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        //public Node prev;
+
+        public Node(int data)
+        {
+            //for not taking garbage value
+            this.next = null;
+            // this.prev = null;
+            this.data = data;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace EmployeeWage
+﻿using System.Collections.Generic;
+
+namespace StackAndQueue
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Stacks and Queues Program");
+            LinkedListQueue list = new LinkedListQueue();
+
+            list.Enqueue(56);
+            list.Enqueue(30);
+            list.Enqueue(70);
+
+            list.Display();
+
+            while (!list.IsEmpty())
+            {
+                list.Dequeue();
+            }
         }
     }
 }
