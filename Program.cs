7597a6fd0c9bb6c35f258b6de98eb53786662ff7
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-21 09:13:01 +0530

    peak and pop from the Stack till it is empty

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace EmployeeWage
+﻿using System.Collections.Generic;
+
+namespace StackAndQueue
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Stacks and Queues Program");
+            LinkedListStack list = new LinkedListStack();
+
+            list.Push(56);
+            list.Push(30);
+            list.Push(70);
+
+            list.Display();
+
+            while (!list.IsEmpty())
+            {
+                list.Peek();
+                list.Pop();
+            }
         }
     }
 }
