a07c131e8c019e5727166374794fb4dea4d7d91e
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-21 09:10:41 +0530

    create a stack

---
+++ LinkedListStack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class LinkedListStack
+    {
+        Node head;
+        Node current;
+        public LinkedListStack()
+        {
+            this.head = null;
+        }
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            //no first node added to list
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+
+                }
+                //assigning node to temp->next
+                temp.next = node;
+            }
+            Console.WriteLine("{0} data is inserted \n", node.data);
+
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty\n");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " \n");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
