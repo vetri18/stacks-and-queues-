0463a48e778dc231669e9f787ed6130b38083321
vetrivel <vetrivel.kce@gmail.com>
2022-08-21 08:59:16 +0530

    welcome to stack and queues program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stacks and Queues Program");
+        }
+    }
+}
